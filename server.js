c76dda64c9aa9770ee68a6436160c9cc684c5e49
stefan-nicolae <84786481+stefan-nicolae@users.noreply.github.com>
2023-09-27 20:22:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,53 @@
 const WebSocket = require('ws');
 const http = require('http');
-const ROOMS = {}
-const clients = []
 
-function generateRoomId(length = 8) {
+const rooms = {}
+
+const getRandomColor = () => Math.random() < 0.5 ? "white" : "black";
+
+function generateID(length=10) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let randomId = '';
+  let id = '';
 
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
-    randomId += characters.charAt(randomIndex);
+    id += characters.charAt(randomIndex);
   }
 
-  return "ROOM" + randomId;
+  return id;
+}
+
+function okToJoin(roomID) {
+  const color1 = getRandomColor()
+  const color2 = color1 === "white" ? "black": "white"
+  const colors = [color1, color2]
+  rooms[roomID].forEach((ws, index) => {
+    ws.send(JSON.stringify({
+      response: "roomJoined",
+      value: roomID
+    }))
+    ws.send(JSON.stringify({
+      response: "color",
+      value: colors[index]
+    }))
+  })
+}
+
+function sendDisconnect(roomID) {
+  if(!rooms[roomID]) return
+  rooms[roomID].forEach(ws => {
+    ws.send(JSON.stringify({
+      response: "disconnect"
+    }))
+  })
+}
+
+function sendToPartner(ws, message) {
+  rooms[ws.roomID].forEach(user => {
+    if(user !== ws) {
+      user.send(message)
+    }
+  })
 }
 
 const server = http.createServer((req, res) => {
@@ -22,129 +57,57 @@ const server = http.createServer((req, res) => {
 
 const wss = new WebSocket.Server({ server });
 
-const sendToTheOther = (ws, msg) => {
-  if(ROOMS[ws.roomID]) {
-    ROOMS[ws.roomID].forEach(user => {
-      if(user !== ws) {
-        user.send(JSON.stringify(msg))
-      }})
-  }
-}
-
 wss.on('connection', (ws) => {
   console.log('Client connected');
-  clients.push(ws)
 
   ws.on('message', (message) => {
     console.log(`Received: ${message}`);
   
     message = JSON.parse(message)
-
     switch(message.request) {
-      case "requestRoomID": 
-        const roomID = generateRoomId()
-        ws.send(JSON.stringify({
-          "response": "roomID",
-          "value": roomID
-        }))
+      case "requestRoomID":
+        var roomID = generateID()
+        rooms[roomID] = [ws]
         ws.roomID = roomID
-        ROOMS[roomID] = [ws]
+        ws.send(JSON.stringify({
+          response: "roomID",
+          value: roomID
+        }))
         break
-      case "joinRoom": 
-        const joiningRoomID = message.value;
-        if(ws.roomID === joiningRoomID) {
-          ws.send(JSON.stringify({
-            "response": "roomError",
-            "error": "You cannot join your own room."
-          })); 
+      case "joinRoom":
+        var roomID = message.value
+        if(rooms[roomID]) {
+          rooms[roomID].push(ws)
+          ws.roomID = roomID
+          okToJoin(roomID)
+        } 
+        break
+      case "URLroomID":
+        var roomID = message.value
+        if(!rooms[roomID]) {
+          rooms[roomID] = [ws]
+          ws.roomID = roomID
         } else {
-          if (ROOMS[joiningRoomID] && ROOMS[joiningRoomID].length === 1) {
-            ROOMS[joiningRoomID].push(ws);
-            ws.roomID = joiningRoomID;
-            
-            ROOMS[joiningRoomID].forEach(user => {
-              user.send(JSON.stringify({
-                "response": "roomJoined",
-                "value": joiningRoomID
-              }))
-            })
-
-            if(!ROOMS[ws.roomID]) return
-            const color = Math.random() < 0.5 ? "white" : "black"
-            ROOMS[ws.roomID][0].send(JSON.stringify({
-              "response": "color",
-              "value": color
-            }))
-            ROOMS[ws.roomID][1].send(JSON.stringify({
-              "response": "color",
-              "value": color === "white" ? "black" : "white"
-            })) 
-          } else {
-            ws.send(JSON.stringify({
-              "response": "roomError",
-              "error": "Room does not exist or is full"
-            }));
-          }
+          rooms[roomID].push(ws)
+          ws.roomID = roomID
+          okToJoin(roomID)
         }
         break
-      case "newChessboard":
-        ROOMS[ws.roomID].forEach(user => {
-          if(user !== ws) {
-            user.send(JSON.stringify({
-              "response": "newChessboard",
-              "value": message.value
-            }))
-          }
-        })
-        break
-      case "capturedPiece":
-        sendToTheOther(ws, {
-          response: "capturedPiece",
+      default: 
+        sendToPartner(ws, JSON.stringify({
+          response: message.request,
           value: message.value
-        })
-        break
-      case "checkmate":
-        ws.send(JSON.stringify({
-          "response": "GAMEOVER",
-          "winner":  message.winner
         }))
-        sendToTheOther(ws, {
-          "response": "GAMEOVER",
-          "winner":  message.winner
-        })
         break
-      }
+      
+    }
   });
 
   ws.on('close', () => {
-    const indexToRemove = clients.indexOf(ws);
-    clients.splice(indexToRemove, 1);    
-    const roomID = ws.roomID
-    console.log(ws.roomID)
-    if (roomID && ROOMS[roomID]) {
-      delete ROOMS[roomID]
-    }
+    sendDisconnect(ws.roomID)
+    delete rooms[ws.roomID]
   });
-});
-
-setInterval(() => {
-  const roomsWithActiveClients = [];
-  for (const roomID in ROOMS) {
-    if (ROOMS.hasOwnProperty(roomID) && ROOMS[roomID].length > 0) {
-      roomsWithActiveClients.push(roomID);
-    }
-  }
-
-  const allExistingRooms = Object.keys(ROOMS);
-
-  for (const roomID of allExistingRooms) {
-    if (!roomsWithActiveClients.includes(roomID)) {
-      delete ROOMS[roomID];
-      console.log(`Room ${roomID} deleted because it's empty.`);
-    }
-  }
-}, 1000);
-
+})
 
 server.listen(8080, () => {
   console.log('WebSocket server is running on http://localhost:8080');
